e57afd53d2c0047c8605a3ea5e063dbef5bbf978
Artem Moiseev <moiseev@physics.msu.ru>
2023-10-19 13:34:06 +0300

    Initial Commit

---
+++ CryptoTestApp/BackgroundView.swift
@@ -0,0 +1,34 @@
+//
+//  BackgroundView.swift
+//  CryptoTestApp
+//
+//  Created by Artem on 19.10.2023.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        ZStack {
+            Color("bgmain").ignoresSafeArea()
+            
+            RadialGradient(colors: [Color("btn").opacity(0.3), Color("bgmain").opacity(0)], center: .center, startRadius: 50, endRadius: 200)
+                .scaleEffect(1)
+                .offset(x: 120, y: -140)
+                .blur(radius: 70)
+            
+            RadialGradient(colors: [Color("red").opacity(0.3), Color("bgmain").opacity(0)], center: .center, startRadius: 50, endRadius: 200)
+                .scaleEffect(1)
+                .offset(x: -120, y: 100)
+                .blur(radius: 70)
+        }
+        .drawingGroup()
+        .ignoresSafeArea()
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}
---
+++ CryptoTestApp/CryptoTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTestAppApp.swift
+//  CryptoTestApp
+//
+//  Created by Artem on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BackgroundView()
+        }
+    }
+}
